b7189794ea3c5a9cfbcce4c7f0027c795e312285
mytja <mytja@protonmail.com>
2022-11-14 17:25:49 +0100

    Average grade

--- src/Grades.svelte
+++ src/Grades.svelte
@@ -2,6 +2,7 @@
     import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
     import {makeRequest} from "./constants";
     import Grade from "./Widgets/Grade.svelte";
+    import Meta from "@smui/list/src/Meta";
 
     let open = false;
     let grades = [];
@@ -29,27 +30,31 @@
             <Row>
                 <Cell class="sameline">
                     <div style="display:inline-block;">{subject.name}</div>
-                    <!--<div style="display:inline-block; font-size: 20px; float:right; color: gray;">{user.Average.toFixed(2)}</div>-->
+                    <Meta style="display:inline-block; font-size: 20px; float:right;">{subject.average.toFixed(2)}</Meta>
                 </Cell>
                 <Cell class="sameline">
-                    {#each subject[0] as grade}
+                    {#each subject[0]["grades"] as grade}
                         <Grade grade={grade} />
                     {/each}
+                    <Meta style="display:inline-block; font-size: 20px; float:right;">{subject[0]["average"].toFixed(2)}</Meta>
                 </Cell>
                 <Cell class="sameline">
-                    {#each subject[1] as grade}
+                    {#each subject[1]["grades"] as grade}
                         <Grade grade={grade} />
                     {/each}
+                    <Meta style="display:inline-block; font-size: 20px; float:right;">{subject[1]["average"].toFixed(2)}</Meta>
                 </Cell>
                 <Cell class="sameline">
-                    {#each subject[2] as grade}
+                    {#each subject[2]["grades"] as grade}
                         <Grade grade={grade} />
                     {/each}
+                    <Meta style="display:inline-block; font-size: 20px; float:right;">{subject[2]["average"].toFixed(2)}</Meta>
                 </Cell>
                 <Cell class="sameline">
-                    {#each subject[3] as grade}
+                    {#each subject[3]["grades"] as grade}
                         <Grade grade={grade} />
                     {/each}
+                    <Meta style="display:inline-block; font-size: 20px; float:right;">{subject[3]["average"].toFixed(2)}</Meta>
                 </Cell>
             </Row>
         {/each}
