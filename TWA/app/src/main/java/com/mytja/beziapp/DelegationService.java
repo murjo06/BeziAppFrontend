b0a004dfe2864139a63be4d070cb190fd0895b81
mytja <mytja@protonmail.com>
2022-11-18 22:07:11 +0100

    Add TWA

---
+++ TWA/app/src/main/java/com/mytja/beziapp/DelegationService.java
@@ -0,0 +1,14 @@
+package com.mytja.beziapp;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
